ca551ac0ecf22e6829b020e4c727afbf809f03f2
mokon1 <michalina.okon@gmail.com>
2017-09-20 08:38:40 +0200

    added connect.php

---
+++ connect.php
@@ -0,0 +1,7 @@
+<?php
+    $host = mysql_connect("localhost","root");
+    $db_user="root";  //add user, password?
+    $db_password ="";
+    $table='customers';
+    $database = 'customersdatabase';
+?>
