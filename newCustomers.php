7b74f8eb09b03a111e07ec9e8dfc180ccaa29505
mokon1 <michalina.okon@gmail.com>
2017-10-04 21:13:58 +0200

    added validation

--- newCustomers.php
+++ newCustomers.php
@@ -8,24 +8,26 @@
 <body>
      <form class="list" method="POST" action="send.php">
          <fieldset>
-         <h2>Our Customers:</h2>
-         <label>Name:<input type="text" name="name"></label>
-         <label>Mail:<input type="email" name="mail"/></label>
-         <label>Phone:<input type="tel" name="phone"/></label>
-         <label>Product:<input type="text" name="product"/></label>
-         <p>Service:</p><textarea class="service" name="service" cols="20" rows="5"></textarea>
-         <label>Price:<input type="number" name="price"/></label>
-         <label>Date:<input type="date" name="date"/></label>
-         <p>Comment:</p><textarea class="comment" name="comment" rows="5"></textarea>
-         <input type="submit" name="newCustomer" value="ADD NEW CUSTOMERS"/>
-        </fieldset>
+             <h2>Our Customers:</h2>
+             <hr>
+             <label>Name:<input type="text" name="name" id="name"></label>
+             <label>Mail:<input type="email" name="mail" id="email"/></label>
+             <label>Phone:<input type="tel" name="phone" id="phone"/></label>
+             <label>Product:<input type="text" name="product"/></label>
+             <p>Service:</p><textarea class="service" name="service" cols="20" rows="5"></textarea>
+             <label>Price:<input type="number" name="price"/></label>
+             <label>Date:<input type="date" name="date" id="date"/></label>
+             <p>Comment:</p><textarea class="comment" name="comment" rows="5" id="comment"></textarea>
+             <input type="submit" name="newCustomer" value="ADD NEW CUSTOMERS"/>
+         </fieldset>
     </form>
    <a href="listOfCustomers.php"><input type="submit" name="customers" value="ALL CUSTOMERS"/></a>
     
     <?php
-        require("connect.php");
+        require("connection.php");
         $result = mysql_query('SELECT * FROM customers ORDER BY date DESC LIMIT 3');
-
+        
+        echo '<h2>The last 3 added customers (order by date)</h2>';
         echo '<table class="list">
             <tr>
                 <th>Id</th>
@@ -53,6 +55,35 @@
         }
         echo '</table>';
 ?>
+    <script>
+        document.addEventListener("DOMContentLoaded", function(){
+            const button = document.querySelector("input[type='submit']");
+            const name = document.querySelector("#name");
+            const email = document.querySelector("#email");
+            const phone = document.querySelector("#phone");
+            const date = document.querySelector("#date");
+            const comment = document.querySelector("#comment");
+            
+            button.addEventListener("click", (event)=>{
+                if(name.value.length<5){
+                    event.preventDefault();
+                    alert("Please enter name (at least 5 characters)");
+                }else if(email.value.indexOf("@")<0){
+                  event.preventDefault();
+                  alert("Please enter email address with '@'");
+                }else if(phone.value.length < 9){
+                  event.preventDefault();
+                  alert("Please enter phone (at least 9 characters)");
+                }else if(date.value.length == 0){
+                    event.preventDefault();
+                    alert("Please enter date");
+                }else if(comment.value.length ==0){
+                    event.preventDefault();
+                    alert("Please enter your comment");
+                }
+            });         
+        });
+    </script>
 
 </body>
 </html>
