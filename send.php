41a5cd0065d1848739e2e5c33dc1c8830bee59b2
mokon1 <michalina.okon@gmail.com>
2017-10-05 16:30:24 +0200

    updated send.php

--- send.php
+++ send.php
@@ -1,32 +1,79 @@
 <?php
 
     require("connection.php");
-        $name = $_POST['name']; //name of input
-        $mail = $_POST['mail'];
-        $phone = $_POST['phone'];
-        $product = $_POST['product'];
-        $service = $_POST['service'];
-        $price = $_POST['price'];
-        $date = $_POST['date'];
-        $comment = $_POST['comment'];
-
-
-        if($name and ($mail or $phone)) {
-
-            $values = mysql_query("INSERT INTO customers SET 
-                name='{$name}', 
-                mail='{$mail}',
-                phone = '{$phone}',
-                product = '{$product}',
-
-                price = '{$price}',
-                date = '{$date}'
-
+    if($_SERVER['REQUEST_METHOD'] === 'POST') { 
+        $name = "";
+        $mail = "";
+        //$phone = "";
+        $product = "";
+        $service = "";
+        $price = "";
+        $date = "";
+        $comment = "";
         
-            "); //add comment & service
+        if(isset($_POST['name']) === true && strlen(trim($_POST['name'])) >= 5) { 
+            $name = trim($_POST['name']); 
+        } else { 
+            echo('Please enter name (at least 5 characters)');
+        }
+        
+        
+        if(isset($_POST['mail']) === true) { 
+            $mail = trim($_POST['mail']); 
+        } else { 
+            echo('Please enter mail');
+        }
+        
+        
+        if(isset($_POST['phone']) === true && strlen(trim($_POST['phone'])) >= 8) { 
+            $phone = $_POST['phone']; 
+        } else { 
+            echo('Please enter phone (at least 9 characters)');
+        }
+        
+        
+        if(isset($_POST['product']) === true) { 
+            $product = trim($_POST['product']); 
+        }
+        
+        
+        if(isset($_POST['service']) === true) { 
+            $service = trim($_POST['service']); 
+        }
+        
+        
+        if(isset($_POST['price']) === true) { 
+            $price = trim($_POST['price']);
+        }
+        
+        
+        if(isset($_POST['date']) === true) { 
+            $date = $_POST['date']; 
+        } else { 
+            echo('Please enter date');
+        }
+        
+        
+        if(isset($_POST['comment']) === true && strlen(trim($_POST['comment'])) >0) { 
+            $comment = trim($_POST['comment']); 
+        } else { 
+            echo('Please enter your comment');
+        }
+        
+        
+        $values = mysql_query("INSERT INTO customers SET 
+            name='{$name}', 
+            mail='{$mail}',
+            phone = '{$phone}',
+            product = '{$product}',
+            service = '{$service}',     
+            price = '{$price}',
+            date = '{$date}',
+            comment = '{$comment}'
+        ");
 
-            if($values) header('Location: newCustomers.php'); 
-            else echo "Błąd nie udało się dodać nowego rekordu";
-
-        } 
+        if($values) header('Location: newCustomers.php'); 
+        else echo "Error! Something wrong with connection";
+        
+    }
 ?>   
